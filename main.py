df15765ea0434a05c75dc9eedf0725bc59078069
Pavel <pavelbareishin@yandex.ru>
2024-01-27 12:34:16 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import csv
+
+def geenerate_hash(s: str):
+    alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ '
+    d = {l : i for i, l in enumerate(alphabet, 1)}
+    p = 67
+    m = 1e9 + 9
+    hash_value = 0
+    p_pow = 1
+    for c in s:
+        hash_value = (hash_value + d[c]*p_pow) % m
+        p_pow = (p_pow *p) % m
+    return int(hash_value)
+
+student_with_hash = []
+with open('students.csv', encoding = 'utf-8') as file:
+    reader = list(csv.DictReader(file, delimiter = ',', quotechar='"'))
+    for row in reader:
+        row['id'] = geenerate_hash(row['Name'])
+        student_with_hash.append(row)
+
+with open('students_new.csv', 'w', newline = '',encoding = 'utf-8') as file:
+    w = csv.DictWriter(file, fieldnames = ['id', 'Name', 'titleProject_id', 'class', 'score'])
+    w.writeheader()
+    w.writerows(student_with_hash)
